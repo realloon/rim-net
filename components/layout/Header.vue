3a5a78b41df4818a31a6004effe38ef57fc6ca72
sincerajn <sincerajn@outlook.com>
2025-06-27 08:04:45 +0800

    Basic page framework.

---
+++ components/layout/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>header</h1>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/post/1">Post/1</NuxtLink></li>
+        <li><NuxtLink to="/post/2">Post/2</NuxtLink></li>
+        <li><NuxtLink to="/post/3">Post/3</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,4 @@
+<template>
+  <LayoutHeader />
+  <slot />
+</template>
