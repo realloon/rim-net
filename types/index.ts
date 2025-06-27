3e67e1d4347bdf784a5f34e42402bc87334f250e
sincerajn <sincerajn@outlook.com>
2025-06-28 02:29:26 +0800

    Fixed Post interface and updated Comment interface.

--- types/index.ts
+++ types/index.ts
@@ -3,8 +3,18 @@ export interface Post {
   author: string
   title: string
   content: string
-  views: number
   likes: number
+  chats: number
   createdAt: number
   updatedAt: number
+}
+
+export interface Comment {
+  id: string
+  postId: string
+  parentCommentId?: string
+  author: string
+  content: string
+  likes: number
+  createdAt: number
 }
