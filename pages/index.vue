0f7d66fef5ee2a29679b38c9fe6bf02c62c11a17
sincerajn <sincerajn@outlook.com>
2025-06-27 10:24:54 +0800

    Initial post list construction.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Post } from '~/types'
+const { data: posts } = useFetch<Post[]>('http://localhost:9000/posts')
+</script>
 
 <template>
   <h1>首页</h1>
+  <section class="post-list" aria-label="post-list">
+    <template v-for="post in posts">
+      <Post v-bind="{ ...post }" />
+    </template>
+  </section>
 </template>
 
 <style></style>
