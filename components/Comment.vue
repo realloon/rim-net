206f8373ce2eabe49786d83172449341be2367ec
sincerajn <sincerajn@outlook.com>
2025-07-02 00:12:19 +0800

    Add components.

---
+++ components/Comment.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Comment } from '~/types'
+
+const { createdAt } = defineProps<Comment>()
+
+const relativeTime = computed(() =>
+  formatRelativeTime(new Date(createdAt))
+)
+</script>
+
+<template>
+  <article class="comment">
+    <UserHeader :author="author" :date="createdAt" />
+
+    {{ content }}
+
+    <footer></footer>
+  </article>
+</template>
+
+<style></style>
---
+++ components/UserHeader.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+const { date, backable = false } = defineProps<{
+  author: string
+  date: number
+  backable?: boolean
+}>()
+
+const datetime = computed(() => new Date(date).toISOString())
+const relativeTime = computed(() => formatRelativeTime(new Date(date)))
+</script>
+
+<template>
+  <header>
+    <Button
+      v-if="backable"
+      class="back-button"
+      @click="$router.back()"
+      text
+      round
+    >
+      <template #icon>
+        <Icon name="mdi:arrow-left" />
+      </template>
+    </Button>
+
+    <Avatar />
+
+    <div>
+      <span class="author">{{ author }}</span>
+      <span class="dot"></span>
+      <time :datetime="datetime">{{ relativeTime }}</time>
+    </div>
+
+    <Spacer />
+
+    <Button text round>
+      <template #icon>
+        <Icon name="mdi:dots-horizontal" />
+      </template>
+    </Button>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+
+  div {
+    display: flex;
+    align-items: baseline;
+    gap: 0.5em;
+
+    .author {
+      font-weight: bold;
+    }
+
+    .dot,
+    time {
+      opacity: 0.7;
+    }
+
+    .dot {
+      display: inline-block;
+      background: #000;
+      width: 4px;
+      aspect-ratio: 1 / 1;
+      border-radius: 50%;
+      transform: translateY(-0.25em);
+    }
+  }
+}
+</style>
